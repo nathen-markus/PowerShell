74a8f9f4ab0fdfdc085f2681636edab879df8836
nathen-markus <68958045+nathen-markus@users.noreply.github.com>
2020-08-05 10:02:58 -0500

    Add files via upload

---
+++ 14dayExpiringNotice.ps1
@@ -0,0 +1,32 @@
+﻿# ---------------- 
+# Define variables 
+# ---------------- 
+ 
+$To = "QBE-Security.US-BOX@us.qbe.com" 
+$From = "QBE-Security.US-BOX@us.qbe.com" 
+$Subject = "Non-Employee Expiration Date(s) - Report" 
+$Body = "The attached CSV file contains a list of user accounts in the QBEAI domain that are due to expire in the next 14 days." 
+$SMTPServer = "qbe-smtp.qbeai.com" 
+$Date = Get-Date -format yyyyMMdd 
+$ReportName = "\\qbeai.com\depts\Technology\Security\Security Administration\Reporting\Contractors\ContractorReport_$((Get-Date).ToString('MM-dd-yyyy')).csv" 
+ 
+ 
+ 
+# ------------------------------------------------------------------ 
+# Get list of users from Active Directory that will expire in 14 days 
+# ------------------------------------------------------------------ 
+ 
+$UserList = Search-ADAccount -AccountExpiring -UsersOnly -TimeSpan 14.00:00:00 | Sort-Object -Descending AccountExpirationDate 
+ 
+ 
+ 
+# ----------------------------------------------- 
+# Send an email using the variables defined above 
+# ----------------------------------------------- 
+ 
+If ($UserList -eq $null){} 
+Else 
+{ 
+   $UserList | Export-CSV $ReportName -NoTypeInformation
+   Send-MailMessage -To $To -From $From -Subject $Subject -Body $Body -SMTPServer $SMTPServer -Attachments $ReportName 
+} 
---
+++ Remove-NestedGroups.ps1
@@ -0,0 +1,21 @@
+﻿$MainGroup = "SQL-VS8PMISC.Misc.RO"
+
+$nestedGroups = Get-ADGroupMember -Server "qbeai.com" -Identity $MainGroup | Where-Object {$_.objectClass -eq "group"}
+
+foreach ($group in $nestedGroups.Name)
+{
+    Write-Output "Removing $group from $MainGroup"
+    Remove-ADGroupMember -Server "qbeai.com" -Identity $MainGroup -Members $group -Confirm:$false
+
+    $allNestedMembers = Get-ADGroupMember -Server "qbeai.com" -Identity $group -Recursive | Where-Object {$_.objectClass -eq "user"}
+
+    if($allNestedMembers -eq $null)
+    {
+        Write-Output "No members to add from $group"
+    }
+    else {
+        Write-Output "Adding members directly into $MainGroup : `n $($allNestedMembers.Name)"
+        Add-ADGroupMember -Server "qbeai.com" -Identity $MainGroup -Members $allNestedMembers -Confirm:$false
+    }
+
+}
